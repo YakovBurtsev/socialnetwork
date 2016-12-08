2c83091fb73534c0901fe1f1357e396c5e125d80
Yakov Burtsev <yakovburtsev@gmail.com>
2016-12-08 21:41:12 +0300

    Add transaction manager

--- user/src/main/java/ru/yakovburtsev/socialnetwork/user/config/SpringRootConfig.java
+++ user/src/main/java/ru/yakovburtsev/socialnetwork/user/config/SpringRootConfig.java
@@ -2,15 +2,25 @@ package ru.yakovburtsev.socialnetwork.user.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
 
 @Configuration
+@EnableTransactionManagement
 @ComponentScan("ru.yakovburtsev.socialnetwork.user")
 public class SpringRootConfig {
     @Autowired
     DataSource dataSource;
+
+    @Bean
+    PlatformTransactionManager txManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
