536b20c9df3f9ef0e5ab860d8cddc794d373b6ba
Yakov Burtsev <yakovburtsev@gmail.com>
2016-12-08 10:23:56 +0300

    Change registration service

--- core/src/main/java/ru/yakovburtsev/socialnetwork/core/service/RegistrationService.java
+++ core/src/main/java/ru/yakovburtsev/socialnetwork/core/service/RegistrationService.java
@@ -1,10 +1,21 @@
 package ru.yakovburtsev.socialnetwork.core.service;
 
-import ru.yakovburtsev.socialnetwork.core.model.User;
+import ru.yakovburtsev.socialnetwork.core.util.exception.RegistrationException;
 
 /**
  * The interface of registration a user in the social network.
  */
 public interface RegistrationService {
-    boolean register(User user);
+
+    /**
+     * Register a user in the social network.
+     *
+     * @param name must be checked in frontend
+     * @param surname must be checked in frontend
+     * @param email must be checked in frontend
+     * @param password must be checked in frontend
+     * @return {@code true} if a registration is successful
+     * @throws RegistrationException if user with such email is already registered
+     */
+    boolean register(String name, String surname, String email, String password) throws RegistrationException;
 }
