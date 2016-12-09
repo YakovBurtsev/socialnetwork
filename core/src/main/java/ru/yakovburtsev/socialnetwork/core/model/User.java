309f647434b0a387895a8ee9d1f287825298ad70
Yakov Burtsev <yakovburtsev@gmail.com>
2016-12-09 17:52:09 +0300

    Add constructors and builder to User class

--- core/src/main/java/ru/yakovburtsev/socialnetwork/core/model/User.java
+++ core/src/main/java/ru/yakovburtsev/socialnetwork/core/model/User.java
@@ -16,7 +16,29 @@ public class User {
     private String email;
     private String password;
 
-    public User() {
+    private User() {
+    }
+
+    private User(Builder builder) {
+        id = builder.id;
+        name = builder.name;
+        surname = builder.surname;
+        birthday = builder.birthday;
+        sex = builder.sex;
+        city = builder.city;
+        email = builder.email;
+        password = builder.password;
+    }
+
+    public User(User u) {
+        id = u.getId();
+        name = u.getName();
+        surname = u.getSurname();
+        birthday = u.getBirthday();
+        sex = u.getSex();
+        city = u.getCity();
+        email = u.getEmail();
+        password = u.getPassword();
     }
 
     public Long getId() {
@@ -98,5 +120,60 @@ public class User {
                 ", city=" + city +
                 ", email=" + email +
                 ')';
+    }
+
+    public static class Builder {
+        private Long id;
+        private String name;
+        private String surname;
+        private LocalDate birthday;
+        private Sex sex;
+        private String city;
+        private String email;
+        private String password;
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public Builder birthday(LocalDate birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public Builder sex(Sex sex) {
+            this.sex = sex;
+            return this;
+        }
+
+        public Builder city(String city) {
+            this.city = city;
+            return this;
+        }
+
+        public Builder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
     }
 }
