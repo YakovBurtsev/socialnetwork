4ac48a42f5c397367a42364c4d5118eb3e6138be
Yakov Burtsev <yakovburtsev@gmail.com>
2016-12-12 20:47:50 +0300

    Add SpringRootConfig file

---
+++ friends/src/main/java/ru/yakovburtsev/socialnetwork/friends/config/SpringRootConfig.java
@@ -0,0 +1,26 @@
+package ru.yakovburtsev.socialnetwork.friends.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("ru.yakovburtsev.socialnetwork.friends")
+public class SpringRootConfig {
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
