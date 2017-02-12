622c7dde34f6d05732444c2e194517293a9339e2
yakov <yakovburtsev@gmail.com>
2017-02-12 11:38:46 +0300

    Add eureka server. It Service Registry pattern implementation.

---
+++ eureka-server/src/main/java/ru/yakovburtsev/socialnetwork/server/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package ru.yakovburtsev.socialnetwork.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka server with service registry.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
