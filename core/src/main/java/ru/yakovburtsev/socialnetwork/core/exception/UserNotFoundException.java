4974b3b942c4c8766a8a1e8cde55527caae03f49
yakov <yakovburtsev@gmail.com>
2017-04-15 13:18:03 +0300

    UserNotFoundException is serializable

--- core/src/main/java/ru/yakovburtsev/socialnetwork/core/exception/UserNotFoundException.java
+++ core/src/main/java/ru/yakovburtsev/socialnetwork/core/exception/UserNotFoundException.java
@@ -1,9 +1,11 @@
 package ru.yakovburtsev.socialnetwork.core.exception;
 
+import java.io.Serializable;
+
 /**
  * This exception will be thrown if User is not found
  */
-public class UserNotFoundException extends RuntimeException {
+public class UserNotFoundException extends RuntimeException implements Serializable{
     private static final long SerialVersionUID = 1;
 
     public UserNotFoundException(String message) {
